7d051e5e7c0204073c888d125fa8b63ef708997e
Garth Webb <garth.webb@gmail.com>
2025-07-12 08:19:29 -0700

    Add a warn level log helper

--- esp32/Logger/Logger.cpp
+++ esp32/Logger/Logger.cpp
@@ -59,3 +59,17 @@ bool Logger::log_debug(String msg) {
 
 	return syslog->log(LOG_DEBUG, msg.c_str());
 }
+
+bool Logger::log_warn(String msg) {
+	if (!WirelessControl::is_connected) {
+		// Don't log if we don't have a connection
+		Serial.println("WiFi disconnected: Syslog serial fallback [warn]: " + msg);
+		return false;
+	}	
+#ifdef LOG_TO_SYSLOG
+	// Exit here if LOG_TO_SYSLOG is defined and false
+	if (!LOG_TO_SYSLOG) { Serial.printf("[WARN] %s\n", msg.c_str()); return true; }
+#endif	
+
+	return syslog->log(LOG_WARNING, msg.c_str());
+}
--- esp32/Logger/Logger.h
+++ esp32/Logger/Logger.h
@@ -12,6 +12,7 @@ class Logger {
 	void init(const char *server, uint16_t port, const char *hostname, const char *app_name);
 	bool log(String msg);
 	bool log_info(String msg);
+	bool log_warn(String msg);
 	bool log_error(String msg);
 	bool log_debug(String msg);
 };
